d633690cbc4a5cfc6a942c2b51cc2a9fdc5437d2
lauravaro <lauravarodesign@gmail.com>
2024-02-05 16:11:50 +0100

    iteracion3

--- neoland-js-functions.js
+++ neoland-js-functions.js
@@ -14,3 +14,10 @@ function findLongestWord(param) {
             longestWord = param[i]; } } return longestWord; }
 
 console.log(findLongestWord(avengers)); 
+
+// Iteracion #3
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+function sumNumbers(numbers) { 
+    let sum = 0; for(let k = 0; k < numbers.length; k++) { sum += numbers[k]; } return sum; }
+    console.log(sumNumbers(numbers)); 
+
